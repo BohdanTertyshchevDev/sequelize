d8b7d811a67f685723740d82caa5103b8054d49e
Bohdan Tertyshchev <bobterya@gmail.com>
2023-04-04 16:58:51 +0300

    Add controller

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const {Router} = require('express');
+const UserController = require('../controllers/User.controller');
 
 const router = Router();
 
-router.get('/user');
+router.post('/user', UserController.createUser);
+router.get('/users', UserController.findAll);
+router.get('/user', UserController.findByPk);
+router.delete('/user/:id', UserController.deleteByPk);
 
 module.exports = router;
